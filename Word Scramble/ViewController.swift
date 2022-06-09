2348bf085badfe8b0def64be1ee74652cb1af6d3
Nikita <69157423+nktshklv-dev@users.noreply.github.com>
2022-06-09 11:51:35 +0300

    added refresh button

--- Word Scramble/ViewController.swift
+++ Word Scramble/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UITableViewController {
         super.viewDidLoad()
         
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(promptForAnswer))
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .refresh, target: self, action: #selector(startGame))
         
         if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt"){
             if let startWords = try? String(contentsOf: startWordsURL){
@@ -28,7 +29,7 @@ class ViewController: UITableViewController {
         
     }
 
-    func startGame(){
+    @objc func startGame(){
         title = allWords.randomElement()
         usedWords.removeAll(keepingCapacity: true)
         tableView.reloadData()
